5ec80b1774724b45f22dd055cceac999637f68b1
Brett Higgins <brett.higgins@gmail.com>
2022-01-03 21:13:45 -0500

    ci: pass PR ref from actions event payload
    GITHUB_REF is always 'master' on the 'close pull request' action.

--- ci/unpublish-expo-release-channel.sh
+++ ci/unpublish-expo-release-channel.sh
@@ -2,8 +2,8 @@
 
 source $(dirname $0)/common.inc
 
-validate_required_env_vars GITHUB_REF
-channel=$(get_release_channel ${GITHUB_REF})
+github_ref=$1
+channel=$(get_release_channel ${github_ref})
 
 # Here's where we'd unpublish the release channel if we could...
 # ...but expo doesn't provide a way to do that, for some reason.
