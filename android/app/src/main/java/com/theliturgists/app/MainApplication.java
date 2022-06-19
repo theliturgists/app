89f84470b9e8d6fc0eb41f31d45cd7cfbbec6f60
Brett Higgins <brett.higgins@gmail.com>
2022-06-18 23:45:17 -0400

    build: fix detox builds by removing flipper
    Couldn't get it to stop crashing the app on startup.
    Removing it fixed the crashes.

--- android/app/src/main/java/com/theliturgists/app/MainApplication.java
+++ android/app/src/main/java/com/theliturgists/app/MainApplication.java
@@ -52,7 +52,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
   }
 
@@ -62,34 +61,4 @@ public class MainApplication extends Application implements ReactApplication {
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.theliturgists.app.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 }
